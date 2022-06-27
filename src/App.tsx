310e79497557799b31c8b2fc97796ee2d9265306
Felipe Augusto Janotte <f.a.janotte@gmail.com>
2022-06-27 15:36:05 -0300

    Create: Header - Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
+import { Header } from "./components/Header/Header";
 import { Pokedex } from "./components/Pokedex/Pokedex";
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Pokedex />
     </div>
   );
